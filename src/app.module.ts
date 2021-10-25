bba89e278872f768128be0b13da43a546f570e2a
ansunrisein <ansunrisein@gmail.com>
2021-10-25 11:30:02 +0300

    Connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import {APP_GUARD} from '@nestjs/core'
 import {Module} from '@nestjs/common'
+import {TypeOrmModule} from '@nestjs/typeorm'
 import {GraphQLModule} from '@nestjs/graphql'
 import {ConfigModule} from '@nestjs/config'
 import {AuthGlobalGuard} from './auth/AuthGlobalGuard'
@@ -8,6 +9,15 @@ import {AuthService} from './auth/service'
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mongodb',
+        url: process.env.DATABASE_URL,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        entities: ['dist/**/model.js'],
+      }),
+    }),
     GraphQLModule.forRoot({
       introspection: true,
       playground: true,
